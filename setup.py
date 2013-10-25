422d351aedf23cd2d9beee2c91723821730476f4
Michaël Malter <dev@michaelmalter.fr>
2013-10-25 22:41:05 +0200

    Basic module layout

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='hastati',
+	version='0.1',
+    description='Tk interface for the Widukind suite',
+    author='Michaël Malter',
+    author_email='dev@michaelmalter.fr',
+    package_dir={'': 'src'},
+    packages=[''],
+	)
+
